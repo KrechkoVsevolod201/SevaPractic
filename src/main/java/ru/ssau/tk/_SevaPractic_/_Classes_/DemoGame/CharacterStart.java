c598180f5f97e23d4a7ee06aeb24dd97d5963d92
Sharky_001 <71152134+KrechkoVsevolod201@users.noreply.github.com>
2020-09-18 23:33:06 +0400

    added a new function

--- src/main/java/ru/ssau/tk/_SevaPractic_/_Classes_/DemoGame/CharacterStart.java
+++ src/main/java/ru/ssau/tk/_SevaPractic_/_Classes_/DemoGame/CharacterStart.java
@@ -34,5 +34,25 @@ public class CharacterStart {
                 points--;
             }
         }
+        String head1 = "(^_^)", head2 = " (._.)", head3 = "(^ . ^)", body1 = "|(|)| ", body2 = "|(||)|", body3 = "|(.)|", legs1 = "_||_", legs2 = " .||.", legs3 = " ,||,";
+        System.out.println("Choose your look");
+        System.out.print(head1);
+        System.out.print(" ");
+        System.out.print(head2);
+        System.out.print(" ");
+        System.out.print(head3);
+        System.out.println(" ");
+        System.out.print(body1);
+        System.out.print(" ");
+        System.out.print(body2);
+        System.out.print(" ");
+        System.out.print(body3);
+        System.out.println(" ");
+        System.out.print(legs1);
+        System.out.print(" "); System.out.print(legs2);
+        System.out.print(" ");
+        System.out.print(legs3);
+        System.out.print(" ");
+
     }
 }
