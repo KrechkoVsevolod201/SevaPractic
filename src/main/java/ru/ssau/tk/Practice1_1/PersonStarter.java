549444151c76abff5e8b349155ead1bdae8224f4
Sharky_001 <71152134+KrechkoVsevolod201@users.noreply.github.com>
2020-09-22 23:37:54 +0400

    Practice task

---
+++ src/main/java/ru/ssau/tk/Practice1_1/PersonStarter.java
@@ -0,0 +1,19 @@
+package ru.ssau.tk.Practice1_1;
+
+
+
+public class PersonStarter {
+
+
+
+    public static void main(String[] s){
+
+        Person person = new Person("Stas", "Baretskiy", 1972);
+
+        person.getfirstName();
+
+    }
+
+
+
+}
