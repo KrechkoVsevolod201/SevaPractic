227e42d30b1381494281d6e05180b1e4189d3410
Sharky_001 <71152134+KrechkoVsevolod201@users.noreply.github.com>
2020-10-14 23:31:37 +0400

    создал тесты, чуть позже их заполню

---
+++ src/test/java/ru/ssau/tk/RandomCode/MathBoyNextDooorTest.java
@@ -0,0 +1,62 @@
+package ru.ssau.tk.RandomCode;
+
+import static org.testng.Assert.*;
+
+public class MathBoyNextDooorTest {
+
+    @org.testng.annotations.Test
+    public void testSetX() {
+    }
+
+    @org.testng.annotations.Test
+    public void testSetY() {
+    }
+
+    @org.testng.annotations.Test
+    public void testSetZ() {
+    }
+
+    @org.testng.annotations.Test
+    public void testGetX() {
+    }
+
+    @org.testng.annotations.Test
+    public void testGetY() {
+    }
+
+    @org.testng.annotations.Test
+    public void testGetZ() {
+    }
+
+    @org.testng.annotations.Test
+    public void testFunctionSqr() {
+    }
+
+    @org.testng.annotations.Test
+    public void testFunctionSqrt() {
+    }
+
+    @org.testng.annotations.Test
+    public void testFunctionCos() {
+    }
+
+    @org.testng.annotations.Test
+    public void testFunctionTan() {
+    }
+
+    @org.testng.annotations.Test
+    public void testFunctionCot() {
+    }
+
+    @org.testng.annotations.Test
+    public void testFunctionSum() {
+    }
+
+    @org.testng.annotations.Test
+    public void testFunctionMult() {
+    }
+
+    @org.testng.annotations.Test
+    public void testFunctionDivision() {
+    }
+}
