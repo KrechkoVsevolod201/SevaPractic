6148e42cb425215dd4dc252eb594abb7be6e5eb1
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-10-26 23:17:49 +0300

    задание 1_6

--- src/main/java/ru/ssau/tk/Practice1_3/Point.java
+++ src/main/java/ru/ssau/tk/Practice1_3/Point.java
@@ -10,8 +10,14 @@ public class Point {
         this.y = y;
         this.z = z;
     }
+
+    double length() {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+
+    public static void main(String[] args) {
         Point point1 = new Point(1, 2, 3);
         Point point2 = new Point(4, 5, 6);
         Point point3 = new Point(7, 8, 9);
     }
-
+}
