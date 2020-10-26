a3f349a1495eff00195aa8ed113de8e4b062cd62
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-10-26 22:59:03 +0300

    задание 1_4

--- src/main/java/ru/ssau/tk/Practice1_4/Points.java
+++ src/main/java/ru/ssau/tk/Practice1_4/Points.java
@@ -2,35 +2,23 @@ package ru.ssau.tk.Practice1_4;
 import ru.ssau.tk.Practice1_3.Point;
 public class Points {
 
-    private Points(){
+    private Points() {
 
     }
-    public static Point sum(Point per1, Point per2){
+
+    public static Point sum(Point per1, Point per2) {
         return new Point(per1.x + per2.x, per1.y + per2.y, per1.z + per2.z);
     }
-    public static Point subtract(Point per1, Point per2){
+
+    public static Point subtract(Point per1, Point per2) {
         return new Point(per1.x - per2.x, per1.y - per2.y, per1.z - per2.z);
     }
-    public static Point multiply(Point per1, Point per2){
+
+    public static Point multiply(Point per1, Point per2) {
         return new Point(per1.x * per2.x, per1.y * per2.y, per1.z * per2.z);
     }
-    public static Point divide(Point per1, Point per2){
-        return new Point(per1.x / per2.x, per1.y / per2.y, per1.z / per2.z);
-    }
-    public static Point enlarge(Point variableNumber,double c){
-        return new Point(variableNumber.x*c,variableNumber.y*c,variableNumber.z*c);
-    }
-    static Point opposite(Point variableNumber){
-        return new Point(variableNumber.x*-1,variableNumber.y*-1,variableNumber.z*-1);
-    }
-    static Point inverse(Point variableNumber){
-        return new Point(1/variableNumber.x, 1/variableNumber.y, 1/variableNumber.z);
-    }
-    public static double scalarProduct(Point per1, Point per2) {
-        return per1.x * per2.x + per1.y * per2.y + per1.z * per2.z;
-    }
 
-    public static Point vectorProduct(Point per1, Point per2) {
-        return new Point(per1.y * per2.z - per1.z * per2.y, per1.z * per2.x - per1.x * per2.z, per1.x * per2.y - per1.y * per2.x);
+    public static Point divide(Point per1, Point per2) {
+        return new Point(per1.x / per2.x, per1.y / per2.y, per1.z / per2.z);
     }
 }
