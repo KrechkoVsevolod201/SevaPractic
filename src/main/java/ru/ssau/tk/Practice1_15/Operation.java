96fca229cf4fffe249616d2ca7aaa572eff214ad
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-10-31 16:36:37 +0300

    Ctrl + Alt + L

--- src/main/java/ru/ssau/tk/Practice1_15/Operation.java
+++ src/main/java/ru/ssau/tk/Practice1_15/Operation.java
@@ -1,9 +1,9 @@
 package ru.ssau.tk.Practice1_15;
 
 abstract public class Operation {
-        abstract double apply(double number);
+    abstract double apply(double number);
 
-        double applyTripple (double number){
-            return apply(apply(apply(number)));
-        }
+    double applyTripple(double number) {
+        return apply(apply(apply(number)));
+    }
 }
--- src/main/java/ru/ssau/tk/Practice1_15/SqrtOperation.java
+++ src/main/java/ru/ssau/tk/Practice1_15/SqrtOperation.java
@@ -1,6 +1,6 @@
 package ru.ssau.tk.Practice1_15;
 
-public class SqrtOperation extends Operation{
+public class SqrtOperation extends Operation {
     @Override
     double apply(double number) {
         return Math.sqrt(number);
