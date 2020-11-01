8d433f7130d1c8a813345a9a2f822ed766ef977c
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-11-01 21:28:37 +0300

    2_16

---
+++ src/main/java/ru/ssau/tk/Practice1_3/NamedPoint.java
@@ -0,0 +1,24 @@
+package ru.ssau.tk.Practice1_3;
+
+public class NamedPoint extends Point {
+    private String name;
+
+    public NamedPoint(double X, double Y, double Z) {
+        super(X, Y, Z);
+    }
+    public NamedPoint(double X,double Y,double Z,String Name){
+        super(X,Y,Z);
+        this.name=Name;
+    }
+    public NamedPoint(){
+        this(0,0,0,"Origin");
+    }
+
+    public String getName() {
+        return name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
