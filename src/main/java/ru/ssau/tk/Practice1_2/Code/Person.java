7a8f20ee99c48376fb7a5b821a90c7cbd33b8671
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-11-02 21:33:21 +0300

    сделано 1_17

--- src/main/java/ru/ssau/tk/Practice1_2/Code/Person.java
+++ src/main/java/ru/ssau/tk/Practice1_2/Code/Person.java
@@ -1,60 +1,77 @@
 package ru.ssau.tk.Practice1_2.Code;
+import ru.ssau.tk.Practice1_17.Gender;
 
 public class Person {
 
     private String firstName;
     private String lastName;
     private int passportId;
+    Gender gender;
+    public Person(String firstName, String lastName, int passportId, Gender gender){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.passportId = passportId;
+        this.gender = gender;
+    }
+    void setGender(Gender gender) {
+        this.gender = gender;
+    }
 
-    public Person(){
+    Gender getGender() {
+
+        return gender;
+    }
+
+
+    public Person() {
 
     }
 
-    public Person(String firstName){
+    public Person(String firstName) {
         this.firstName = firstName;
     }
 
-    public Person(String firstName, String lastName){
+    public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
-    public Person(int passportId){
+    public Person(int passportId) {
         this.passportId = passportId;
     }
 
-    public Person(String firstName, String lastName, int passportId){
+    public Person(String firstName, String lastName, int passportId) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.passportId = passportId;
     }
 
-    public String getfirstName () {
+    public String getfirstName() {
         System.out.println(firstName);
         return firstName;
     }
 
-    public void setFirstName (String firstName){
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    public String getlastName () {
+    public String getlastName() {
         return lastName;
     }
 
-    public void setlastName (String lastName){
+    public void setlastName(String lastName) {
         this.lastName = lastName;
     }
 
-    public int getpassportId () {
+    public int getpassportId() {
         return passportId;
     }
 
-    public void setpassportId ( int passportId){
+    public void setpassportId(int passportId) {
         this.passportId = passportId;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Person person1 = new Person();
         person1.setFirstName("Stas");
         person1.setlastName("Baretskiy");
@@ -67,7 +84,6 @@ public class Person {
         Person person3 = new Person("Killer");
         Person person4 = new Person("Sosiska");
         Person person5 = new Person(666);
-
 
     }
 }
