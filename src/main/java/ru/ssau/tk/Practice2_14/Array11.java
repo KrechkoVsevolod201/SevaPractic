197e9ebecf0297ab7f949c70a42148dc718adaf0
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-11-16 22:39:21 +0300

    2_14

---
+++ src/main/java/ru/ssau/tk/Practice2_14/Array11.java
@@ -0,0 +1,10 @@
+package ru.ssau.tk.Practice2_14;
+
+public class Array11 {
+    static void massiv(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = -((Integer) array[i]);
+
+        }
+    }
+}
