bae79f0a927e8f593ec111cce573bd9a97d4a9e0
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-12-13 19:31:36 +0300

    3_6

---
+++ src/main/java/ru/ssau/tk/Practice3_6/Task3_6.java
@@ -0,0 +1,16 @@
+package ru.ssau.tk.Practice3_6;
+
+public class Task3_6 {
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println("Символ \t№" + ++i);
+        System.out.println("Символ \b№" + ++i);
+        System.out.println("Символ \n№" + ++i);
+        System.out.println("Символ \r№" + ++i);
+        System.out.println("Символ \'№" + ++i);
+        System.out.println("Символ \"№" + ++i);
+        System.out.println("Символ\\№" + ++i);
+
+    }
+}
+//для того, чтобы понять, что после \ есть команда
