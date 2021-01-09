3a23b9d9a19760dbbd3427b08beb69c506a45c48
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2021-01-09 20:01:07 +0300

    3_19

---
+++ src/main/java/ru/ssau/tk/Practice3_19/Task3_19.java
@@ -0,0 +1,22 @@
+package ru.ssau.tk.Practice3_19;
+
+import java.util.Arrays;
+
+public class Task3_19 {
+    static String[] string(String str) {
+        String[] arrayStr = str.split(" ");
+        return Arrays.stream(arrayStr)
+                .filter(s -> !s.isEmpty())
+                .map(s -> s.replace(
+                        String.valueOf(s.charAt(0)),
+                        String.valueOf(s.charAt(0)).toUpperCase()
+                ))
+                .toArray(String[]::new);
+        //        for (int i = 0; i < arrayStr.length; i++) {
+//            if (!arrayStr[i].isEmpty()) {
+//                arrayStr[i] = arrayStr[i].substring(0, 1).toUpperCase() + arrayStr[i].substring(1);
+//            }
+//        }
+//        return arrayStr;
+    }
+}
