8c3338f0cb2adef5d454f4976e130fe04c645722
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2021-01-09 20:57:38 +0300

    3_23

---
+++ src/main/java/ru/ssau/tk/Practice3_23/Task3_23.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.Practice3_23;
+
+import java.nio.charset.Charset;
+
+public class Task3_23 {
+    public static void main(String[] args) {
+        System.out.println(Charset.defaultCharset());
+    }
+}
