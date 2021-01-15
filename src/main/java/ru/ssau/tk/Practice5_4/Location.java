f8f7a3c51e89eecb300d84f5e8d409912d7f315d
Sharky_001 <71152134+KrechkoVsevolod201@users.noreply.github.com>
2021-01-15 12:25:35 +0400

    5.4 complete

---
+++ src/main/java/ru/ssau/tk/Practice5_4/Location.java
@@ -0,0 +1,57 @@
+package ru.ssau.tk.Practice5_4;
+
+import java.util.Objects;
+
+public class Location {
+    private int id;
+    private String name;
+    private double latitude;
+    private double longitude;
+
+    public Location() {
+
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        } else if (object == null || getClass() != object.getClass()) { return false; }
+        Location location = (Location) object;
+        return id == location.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
